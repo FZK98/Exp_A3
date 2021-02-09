1eef0d982b4a390024c2c4e7a0771dc0125ad648
mariawood98 <63402347+mariawood98@users.noreply.github.com>
2021-02-09 12:40:53 +0000

    Add files via upload

--- task5.py
+++ task5.py
@@ -12,24 +12,25 @@ import matplotlib.pyplot as plt
 wl, n, k=np.loadtxt('BK7.txt', delimiter = '\t', skiprows=1, unpack=True ) 
 
 #define the sellmeier refractive index function
-def sellmeier(wavelength, a1,a2, a3,b1, b2,b3):
-	tempFraction1 = a1*(wavelength**2)/(wavelength**2-b1**2)
-	tempFraction2=a2*(wavelength**2)/(wavelength**2-b2**2)
-	tempFraction3=a3*(wavelength**2)/(wavelength**2-b3**2)
+def sellmeier_fit(wavelength, a1,a2, a3,b1, b2,b3):
+	wavelength=wavelength/1000
+	tempFraction1 = a1*(wavelength**2)/(wavelength**2-b1)
+	tempFraction2=a2*(wavelength**2)/(wavelength**2-b2)
+	tempFraction3=a3*(wavelength**2)/(wavelength**2-b3)
 	return np.sqrt(1+tempFraction1+tempFraction2+tempFraction3)
 
 #fit the sellmeier function to the data with initial guesses of coefficients
-popt, _ = sp.optimize.curve_fit(sellmeier, wl, n, [0.39, 0.45, 0.39,116.6, 116.6, -116]) 
+popt, _ = sp.optimize.curve_fit(sellmeier_fit, wl, n, [1.04, 0.23, 1.01, 0.006, 0.02, 103.5]) #starting estimates from wikipedia
 
 #make list of refractive indices for each wl using sellmeier equation with fitted coefficients
 sellmeiery = []
 for i in wl:
-	tempN=sellmeier(i, popt[0],popt[1],popt[2],popt[3],popt[4],popt[5])
+	tempN=sellmeier_fit(i, popt[0],popt[1],popt[2],popt[3],popt[4],popt[5])
 	sellmeiery.append(tempN)
 	
 #plot results
 plt.figure()
-plt.plot(wl,n, label='data')
+plt.plot(wl,n, 'x', label='data')
 plt.plot(wl, sellmeiery, label='sellmeier 3 terms')
 plt.legend()
 plt.grid()
