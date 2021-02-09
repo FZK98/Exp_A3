94df34c90af5774470b1b069c439aab543ad0f13
mariawood98 <63402347+mariawood98@users.noreply.github.com>
2021-02-09 11:11:49 +0000

    Add files via upload

--- task5.py
+++ task5.py
@@ -12,34 +12,32 @@ import matplotlib.pyplot as plt
 wl, n, k=np.loadtxt('BK7.txt', delimiter = '\t', skiprows=1, unpack=True ) 
 
 #define the sellmeier refractive index function
-def sellmeier(wavelength, a1, a2, a3, a4, a5, b1, b2, b3, b4, b5):
+def sellmeier(wavelength, a1,a2, a3,b1, b2,b3):
 	tempFraction1 = a1*(wavelength**2)/(wavelength**2-b1**2)
 	tempFraction2=a2*(wavelength**2)/(wavelength**2-b2**2)
 	tempFraction3=a3*(wavelength**2)/(wavelength**2-b3**2)
-	tempFraction4=a4*(wavelength**2)/(wavelength**2-b4**2)
-	tempFraction5=a5*(wavelength**2)/(wavelength**2-b5**2)
-	return np.sqrt(1+tempFraction1+tempFraction2+tempFraction3+tempFraction4+tempFraction5)
+	return np.sqrt(1+tempFraction1+tempFraction2+tempFraction3)
 
 #fit the sellmeier function to the data with initial guesses of coefficients
-popt, _ = sp.optimize.curve_fit(sellmeier, wl, n, [0.39, 0.45, 0.39, 0.3,0.3, 116.6,116.6,-116.6,-100,-100])
+popt, _ = sp.optimize.curve_fit(sellmeier, wl, n, [0.39, 0.45, 0.39,116.6, 116.6, -116]) 
 
 #make list of refractive indices for each wl using sellmeier equation with fitted coefficients
 sellmeiery = []
 for i in wl:
-	tempN=sellmeier(i, popt[0],popt[1],popt[2],popt[3],popt[4],popt[5],popt[6],popt[7], popt[8],popt[9])
+	tempN=sellmeier(i, popt[0],popt[1],popt[2],popt[3],popt[4],popt[5])
 	sellmeiery.append(tempN)
 	
 #plot results
 plt.figure()
 plt.plot(wl,n, label='data')
-plt.plot(wl, sellmeiery, label='sellmeier 5 terms')
+plt.plot(wl, sellmeiery, label='sellmeier 3 terms')
 plt.legend()
 plt.grid()
-a_BK7 = popt[0:4]
-b_BK7 = popt[5:9]
-print("the sellmeier coefficeints are A: ["+str(a_BK7)+"], B:["+str(b_BK7)+"].")
+#a_BK7 = popt[0:2]
+#b_BK7 = popt[3:5]
+#print("the sellmeier coefficeints are A: ["+str(a_BK7)+"], B:["+str(b_BK7)+"].")
 
 #plot difference in results
-plt.figure()
-plt.plot(wl, (sellmeiery-n))
-plt.grid()
+#plt.figure()
+#plt.plot(wl, (sellmeiery-n))
+#plt.grid()
